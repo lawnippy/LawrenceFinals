20162d4e9b09af8dd5a0caaf6a21fe312f9fc867
lawnippy <lawrence.osegbe@valpo.edu>
2021-05-14 14:02:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lawrencefinals/db/TaskContract.java
@@ -0,0 +1,15 @@
+package com.example.lawrencefinals.db;
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.lawrencefinals.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
+
